24a49d143aa5467b8fd379739365ad7c8e5e455a
Joao Marcos <joaomarcos11.tech@gmail.com>
2023-03-01 21:26:02 -0300

    bar and pie chart

--- src/pages/BarChart/index.tsx
+++ src/pages/BarChart/index.tsx
@@ -39,3 +39,75 @@ const BarChart = (): JSX.Element => {
 }
 
 export default BarChart;
+
+// export const data = [
+//   ["City", "2010 Population", "2000 Population"],
+//   ["New York City, NY", 8175000, 8008000],
+//   ["Los Angeles, CA", 3792000, 3694000],
+//   ["Chicago, IL", 2695000, 2896000],
+//   ["Houston, TX", 2099000, 1953000],
+//   ["Philadelphia, PA", 1526000, 1517000],
+// ];
+
+// export const options = {
+//   title: "Population of Largest U.S. Cities",
+//   chartArea: { width: "50%" },
+//   isStacked: true,
+//   hAxis: {
+//     title: "Total Population",
+//     minValue: 0,
+//   },
+//   vAxis: {
+//     title: "City",
+//   },
+// };
+
+// export function App() {
+//   return (
+//     <Chart
+//       chartType="BarChart"
+//       width="100%"
+//       height="400px"
+//       data={data}
+//       options={options}
+//     />
+//   );
+// }
+
+// export const data = [
+//   [
+//     "Element",
+//     "Density",
+//     { role: "style" },
+//     {
+//       sourceColumn: 0,
+//       role: "annotation",
+//       type: "string",
+//       calc: "stringify",
+//     },
+//   ],
+//   ["Copper", 8.94, "#b87333", null],
+//   ["Silver", 10.49, "silver", null],
+//   ["Gold", 19.3, "gold", null],
+//   ["Platinum", 21.45, "color: #e5e4e2", null],
+// ];
+
+// export const options = {
+//   title: "Density of Precious Metals, in g/cm^3",
+//   width: 600,
+//   height: 400,
+//   bar: { groupWidth: "95%" },
+//   legend: { position: "none" },
+// };
+
+// export function App() {
+//   return (
+//     <Chart
+//       chartType="BarChart"
+//       width="100%"
+//       height="400px"
+//       data={data}
+//       options={options}
+//     />
+//   );
+// }
--- src/pages/PieChart/index.tsx
+++ src/pages/PieChart/index.tsx
@@ -1,11 +1,109 @@
 import React from "react";
+import Chart from "react-google-charts";
 
 const PieChart = (): JSX.Element => {
+    const data = [
+        ["Task", "Hours per Day"],
+        ["Work", 11],
+        ["Eat", 2],
+        ["Commute", 2],
+        ["Watch TV", 2],
+        ["Sleep", 7],
+    ];
+      
+    const options = {
+        title: "My Daily Activities",
+    };
+
     return (
         <>
             <h1>Pie Chart</h1>
+            <Chart
+                chartType="PieChart"
+                data={data}
+                options={options}
+                width={"100%"}
+                height={"400px"}
+            />
         </>
     )
 }
 
 export default PieChart;
+
+// export const data = [
+//     ["Language", "Speakers (in millions)"],
+//     ["Assamese", 13],
+//     ["Bengali", 83],
+//     ["Bodo", 1.4],
+//     ["Dogri", 2.3],
+//     ["Gujarati", 46],
+//     ["Hindi", 300],
+//     ["Kannada", 38],
+//     ["Kashmiri", 5.5],
+//     ["Konkani", 5],
+//     ["Maithili", 20],
+//     ["Malayalam", 33],
+//     ["Manipuri", 1.5],
+//     ["Marathi", 72],
+//     ["Nepali", 2.9],
+//     ["Oriya", 33],
+//     ["Punjabi", 29],
+//     ["Sanskrit", 0.01],
+//     ["Santhali", 6.5],
+//     ["Sindhi", 2.5],
+//     ["Tamil", 61],
+//     ["Telugu", 74],
+//     ["Urdu", 52],
+//   ];
+  
+//   export const options = {
+//     title: "Indian Language Use",
+//     legend: "none",
+//     pieSliceText: "label",
+//     slices: {
+//       4: { offset: 0.2 },
+//       12: { offset: 0.3 },
+//       14: { offset: 0.4 },
+//       15: { offset: 0.5 },
+//     },
+//   };
+  
+//   export function App() {
+//     return (
+//       <Chart
+//         chartType="PieChart"
+//         data={data}
+//         options={options}
+//         width={"100%"}
+//         height={"400px"}
+//       />
+//     );
+//   }
+
+
+// export const data = [
+//     ["Pizza", "Popularity"],
+//     ["Pepperoni", 33],
+//     ["Hawaiian", 26],
+//     ["Mushroom", 22],
+//     ["Sausage", 10], // Below limit.
+//     ["Anchovies", 9], // Below limit.
+//   ];
+  
+//   export const options = {
+//     title: "Popularity of Types of Pizza",
+//     sliceVisibilityThreshold: 0.2, // 20%
+//   };
+  
+//   export function App() {
+//     return (
+//       <Chart
+//         chartType="PieChart"
+//         data={data}
+//         options={options}
+//         width={"100%"}
+//         height={"400px"}
+//       />
+//     );
+//   }
